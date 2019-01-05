b431818e833eab6a74d18e17cb385c6785572b80
Ben Harwood <ben.m.harwood@gmail.com>
2019-01-05 15:12:41 -0600

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash -e
+
+VENV=venv
+
+if [ ! -d "$VENV"]
+then
+
+	PYTHON=`which python2`
+
+	if [ ! -f $PYTHON ]
+	then
+		echo "Python 2 not found"
+	fi
+
+	virtualenv -p $PYTHON $VENV
+fi
+
+. $VENV/bin/activate
+
+pip install -r requirements.txt
